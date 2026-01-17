974ff0abd997c86a76ad3854fc417be8c54c8d7c
你的姓名 <you@example.com>
2026-01-17 13:55:22 +0800

    清理构建产物和缓存文件

--- patch-native/build/intermediates/cxx/Debug/2y4w1g4p/logs/x86/build_command_patchengine.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\android\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\android\\projecet_iade\\androidhotupdate\\patch-native\\.cxx\\Debug\\2y4w1g4p\\x86" ^
-  patchengine
--- patch-native/build/intermediates/cxx/RelWithDebInfo/1716y6dg/logs/armeabi-v7a/build_command_patchengine.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\android\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\android\\projecet_iade\\androidhotupdate\\patch-native\\.cxx\\RelWithDebInfo\\1716y6dg\\armeabi-v7a" ^
-  patchengine
